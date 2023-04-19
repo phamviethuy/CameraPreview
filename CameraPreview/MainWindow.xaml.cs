a94d2ca6a53038d5fff06b7511bdcc0deddabd91
Huy <phamviethuy371@gmail.com>
2023-04-19 18:01:50 +0700

    Add project files.

---
+++ CameraPreview/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CameraPreview
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private VideoCapture capture;
+        private readonly PreviewImage previewImage;
+        public MainWindow()
+        {
+            InitializeComponent();
+            previewImage = new PreviewImage(1920, 1080);
+            ImgContainer.Children.Add(previewImage.GetControl());
+            InitDevs();
+        }
+        private void InitDevs()
+        {
+            capture = new VideoCapture(0,VideoCapture.API.Ffmpeg);
+            capture.SetCaptureProperty(CapProp.FrameWidth, 1920);
+            capture.SetCaptureProperty(CapProp.FrameHeight, 1080);
+            Preview();
+            capture.Start();
+        }
+
+        /// <summary>
+        /// Previews on UI.
+        /// </summary>
+        private void Preview()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    var isGrabed = capture.Grab();
+                    if (isGrabed)
+                    {
+                        var image = new Image<Bgr, byte>(1920, 1080);
+                        var isRetrieved = capture.Retrieve(image);
+                        if (isRetrieved)
+                        {
+                            previewImage.Render(image);
+                        }
+                        else
+                        {
+                            image.Dispose();
+                        }
+                    }
+
+                    Thread.Sleep(40);
+                }
+            });
+        }
+
+    }
+}
---
+++ CameraPreview/PreviewImage.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Controls;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace CameraPreview
+{
+    internal class PreviewImage
+    {
+        public Image Control { get; private set; }
+        private readonly WriteableBitmap imageSource;
+
+        int w, h;
+        public PreviewImage(int w, int h)
+        {
+            this.w = w;
+            this.h = h;
+            Control = new Image();
+            imageSource = new WriteableBitmap(w, h, 96, 96, PixelFormats.Bgr24, null);
+            Control.Source = imageSource;
+        }
+
+        public void Render(Image<Bgr, byte> imageCV)
+        {
+            try
+            {
+                if (!Control.Dispatcher.CheckAccess())
+                {
+                    Control.Dispatcher.Invoke(() =>
+                    {
+                        Render(imageCV);
+                    });
+
+                    return;
+                }
+                using var newImg = imageCV.Resize(w, h, Emgu.CV.CvEnum.Inter.Linear);
+                imageSource.Lock();
+                imageSource?.WritePixels(new Int32Rect(0, 0, w, h), newImg.MIplImage.ImageData, newImg.MIplImage.ImageSize, newImg.MIplImage.WidthStep);
+                imageSource.Unlock();
+            }
+            catch
+            {
+            }
+        }
+
+        public Image GetControl()
+        {
+            return Control;
+        }
+    }
+}
